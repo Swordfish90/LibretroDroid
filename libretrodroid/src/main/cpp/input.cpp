dea75a9189b90e3da3905ccf4aa2694123f11f46
Filippo Scognamiglio <flscogna@gmail.com>
2020-10-26 02:37:15 +0100

    Allow negative ports, but clamp values.

--- libretrodroid/src/main/cpp/input.cpp
+++ libretrodroid/src/main/cpp/input.cpp
@@ -25,7 +25,7 @@
 #include "libretro/libretro.h"
 
 int16_t LibretroDroid::Input::getInputState(unsigned port, unsigned device, unsigned index, unsigned id) {
-    if (port >= 4 || port < 0) return 0;
+    port = std::min(std::max(0u, port), 4u);
 
     switch (device) {
         case RETRO_DEVICE_JOYPAD: {
