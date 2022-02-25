6123cdc3814cc241d253d3409dac80820684094f
Filippo Scognamiglio <flscogna@gmail.com>
2022-02-25 08:08:49 +0100

    Add texture clamp to framebuffer renderer.

--- libretrodroid/src/main/cpp/renderers/es3/framebufferrenderer.cpp
+++ libretrodroid/src/main/cpp/renderers/es3/framebufferrenderer.cpp
@@ -57,6 +57,8 @@ void FramebufferRenderer::updateRenderedResolution(unsigned int width, unsigned 
     glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA8, width, height);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, currentTexture, 0);
 
     if (depth) {
