85f012db88aec3015ce8767bc699dae526cd8579
Filippo Scognamiglio <flscogna@gmail.com>
2022-05-19 22:38:09 +0200

    Use contrast aware sharpening to improve behaviour on a variety of cases.

--- libretrodroid/src/main/cpp/shadermanager.cpp
+++ libretrodroid/src/main/cpp/shadermanager.cpp
@@ -173,6 +173,8 @@ const std::string ShaderManager::diamondUpscaleFragment =
     "precision mediump float;\n"
     "#endif\n"
     "\n"
+    "#define STATIC_SMOOTHING 0.2\n"
+    "\n"
     "precision mediump float;\n"
     "uniform lowp sampler2D texture;\n"
     "uniform HIGHP vec2 textureSize;\n"
@@ -183,27 +185,27 @@ const std::string ShaderManager::diamondUpscaleFragment =
     "lowp float luma(lowp vec3 v) {\n"
     "  return dot(v, vec3(0.21, 0.72, 0.04));\n"
     "}\n"
-    "lowp float sharpSmooth(float t) {"
-    "  return smoothstep(0.5 - SMOOTHNESS, 0.5 + SMOOTHNESS, t);"
+    "lowp float sharpSmooth(lowp float t, lowp float smoothness) {"
+    "  return smoothstep(0.5 - smoothness, 0.5 + smoothness, t);"
     "}\n"
-    "lowp vec3 quadBilinear(lowp vec3 a, lowp vec3 b, lowp vec3 c, lowp vec3 d, lowp vec2 p) {\n"
-    "  lowp float x = sharpSmooth(p.x);\n"
-    "  lowp float y = sharpSmooth(p.y);\n"
+    "lowp vec3 quadBilinear(lowp vec3 a, lowp vec3 b, lowp vec3 c, lowp vec3 d, lowp vec2 p, lowp float smoothness) {\n"
+    "  lowp float x = sharpSmooth(p.x, smoothness);\n"
+    "  lowp float y = sharpSmooth(p.y, smoothness);\n"
     "  return mix(mix(a, b, x), mix(c, d, x), y);\n"
     "}\n"
     "// Fast computation of barycentric coordinates only in the sub-triangle 1 2 4\n"
-    "lowp vec3 fastBarycentric(lowp vec2 p) {\n"
-    "  lowp float l0 = sharpSmooth(1.0 - p.x - p.y);\n"
-    "  lowp float l1 = sharpSmooth(p.x);\n"
+    "lowp vec3 fastBarycentric(lowp vec2 p, lowp float smoothness) {\n"
+    "  lowp float l0 = sharpSmooth(1.0 - p.x - p.y, smoothness);\n"
+    "  lowp float l1 = sharpSmooth(p.x, smoothness);\n"
     "  return vec3(l0, l1, 1.0 - l0 - l1);\n"
     "}\n"
     "\n"
-    "lowp vec3 triangleInterpolate(lowp vec3 t1, lowp vec3 t2, lowp vec3 t3, lowp vec3 t4, lowp vec2 c) {\n"
+    "lowp vec3 triangleInterpolate(lowp vec3 t1, lowp vec3 t2, lowp vec3 t3, lowp vec3 t4, lowp vec2 c, lowp float smoothness) {\n"
     "  // Alter colors and coordinates to compute the other triangle.\n"
     "  bool altTriangle = 1.0 - c.x < c.y;\n"
     "  lowp vec3 cornerColor = altTriangle ? t3 : t1;\n"
     "  lowp vec2 triangleCoords = altTriangle ? vec2(1.0 - c.y, 1.0 - c.x) : c;\n"
-    "  lowp vec3 weights = fastBarycentric(triangleCoords);\n"
+    "  lowp vec3 weights = fastBarycentric(triangleCoords, smoothness);\n"
     "  return weights.x * cornerColor + weights.y * t2 + weights.z * t4;\n"
     "}\n"
     "\n"
@@ -219,10 +221,23 @@ const std::string ShaderManager::diamondUpscaleFragment =
     "  lowp vec3 t3 = texture2D(texture, c3).rgb;\n"
     "  lowp vec3 t4 = texture2D(texture, c4).rgb;\n"
     "\n"
+    "  lowp float l1 = luma(t1);\n"
+    "  lowp float l2 = luma(t2);\n"
+    "  lowp float l3 = luma(t3);\n"
+    "  lowp float l4 = luma(t4);\n"
+    "\n"
+    "#if USE_DYNAMIC_SMOOTHING\n"
+    "  lowp float contrast = max(max(l1, l2), max(l3, l4)) - min(min(l1, l2), min(l3, l4));\n"
+    "  contrast = smoothstep(DYNAMIC_SMOOTHING_MIN_CONTRAST, DYNAMIC_SMOOTHING_MAX_CONTRAST, contrast);\n"
+    "  lowp float smoothness = mix(DYNAMIC_SMOOTHING_MAX, DYNAMIC_SMOOTHING_MIN, contrast);\n"
+    "#else\n"
+    "  const lowp float smoothness = STATIC_SMOOTHING;\n"
+    "#endif\n"
+    "\n"
     "  lowp vec2 pxCoords = fract(screenCoords);\n"
     "\n"
-    "  lowp float diagonal1Strength = abs(luma(t1) - luma(t3));\n"
-    "  lowp float diagonal2Strength = abs(luma(t2) - luma(t4));\n"
+    "  lowp float diagonal1Strength = abs(l1 - l3);\n"
+    "  lowp float diagonal2Strength = abs(l2 - l4);\n"
     "\n"
     "  // Alter colors and coordinates to compute the other triangulation.\n"
     "  bool altTriangulation = diagonal1Strength < diagonal2Strength;\n"
@@ -232,18 +247,18 @@ const std::string ShaderManager::diamondUpscaleFragment =
     "    altTriangulation ? t3 : t2,\n"
     "    altTriangulation ? t4 : t3,\n"
     "    altTriangulation ? t1 : t4,\n"
-    "    altTriangulation ? vec2(pxCoords.y, 1.0 - pxCoords.x) : pxCoords\n"
+    "    altTriangulation ? vec2(pxCoords.y, 1.0 - pxCoords.x) : pxCoords,\n"
+    "    smoothness\n"
     "  );\n"
     "\n"
     "  lowp float d1 = step(diagonal2Strength * 3.0 + 0.05, diagonal1Strength);\n"
     "  lowp float d2 = step(diagonal1Strength * 3.0 + 0.05, diagonal2Strength);\n"
     "\n"
-    "  lowp vec3 final = quadBilinear(t1, t2, t4, t3, pxCoords);\n"
+    "  lowp vec3 final = quadBilinear(t1, t2, t4, t3, pxCoords, smoothness);\n"
     "  final = d1 + d2 > 0.5 ? cd : final;\n"
     "\n"
     "  gl_FragColor = vec4(final, 1.0);\n"
     "}\n";
-
 
 ShaderManager::Data ShaderManager::getShader(Type type) {
     switch (type) {
@@ -260,10 +275,28 @@ ShaderManager::Data ShaderManager::getShader(Type type) {
         return { defaultShaderVertex, defaultSharpFragment, true };
 
     case Type::SHADER_DIAMOND_UPSCALE_SHARP:
-        return { diamondUpscaleVertex, "#define SMOOTHNESS 0.2\n\n" + diamondUpscaleFragment, false };
+        return {
+            diamondUpscaleVertex,
+            "#define USE_DYNAMIC_SMOOTHING 1\n"
+            "#define DYNAMIC_SMOOTHING_MIN_CONTRAST 0.20\n"
+            "#define DYNAMIC_SMOOTHING_MAX_CONTRAST 0.80\n"
+            "#define DYNAMIC_SMOOTHING_MIN 0.20\n"
+            "#define DYNAMIC_SMOOTHING_MAX 0.40\n"
+            + diamondUpscaleFragment,
+            false
+        };
 
     case Type::SHADER_DIAMOND_UPSCALE_SMOOTH:
-        return { diamondUpscaleVertex, "#define SMOOTHNESS 0.4\n\n" + diamondUpscaleFragment ,false };
+        return {
+            diamondUpscaleVertex,
+            "#define USE_DYNAMIC_SMOOTHING 1\n"
+            "#define DYNAMIC_SMOOTHING_MIN_CONTRAST 0.20\n"
+            "#define DYNAMIC_SMOOTHING_MAX_CONTRAST 0.80\n"
+            "#define DYNAMIC_SMOOTHING_MIN 0.25\n"
+            "#define DYNAMIC_SMOOTHING_MAX 0.50\n"
+            + diamondUpscaleFragment ,
+            false
+        };
     }
 }
 
