7aef66150d773730d593c3d1a343aee350d85f7c
Filippo Scognamiglio <flscogna@gmail.com>
2024-09-12 12:48:05 +0200

    Integrate upstream changes.

--- libretrodroid/src/main/java/com/swordfish/libretrodroid/ShaderConfig.kt
+++ libretrodroid/src/main/java/com/swordfish/libretrodroid/ShaderConfig.kt
@@ -60,7 +60,7 @@ sealed interface ShaderConfig {
         val edgeMinValue: Float = 0.025f,
         val softEdgesSharpening: Boolean = true,
         val softEdgesSharpeningAmount: Float = 1.0f,
-        val searchMinContrast: Float = 0.75f,
+        val searchMinContrast: Float = 0.85f,
         val searchMaxDistance: Int = 4,
     ) : ShaderConfig
 }
